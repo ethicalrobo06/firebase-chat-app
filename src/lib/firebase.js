417d2e87664687888d2ecffb27ead4c555707ee1
Chandan <ethicalrobo06@gmail.com>
2024-07-05 20:34:12 +0530

    modified env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,13 +5,12 @@ import { getStorage } from 'firebase/storage'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_API_KEY,
-  apiKey: 'AIzaSyDQ3-fYoDnvr1He1cj8cAkYzwhy3eWu1XI',
-  authDomain: 'reactchat-575c2.firebaseapp.com',
-  projectId: 'reactchat-575c2',
-  storageBucket: 'reactchat-575c2.appspot.com',
-  messagingSenderId: '557797347728',
-  appId: '1:557797347728:web:107311dad3a91e2e4a2315',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
